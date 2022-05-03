724abae92f44ea1e1ba7012699c75f1f8ee0ecf9
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-05-03 01:13:49 -0500

    hover working

---
+++ project_1/app.js
@@ -0,0 +1,17 @@
+
+// hover function isn't working.
+
+$(document).ready(function(){
+    $('.box').hover(function(){
+
+        $(this).css("background-color", "black")
+    },function(){
+      $(this).css("background-color", "white")
+    })
+})
+
+
+(() => {
+
+
+})
